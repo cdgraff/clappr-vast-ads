f5dd8f6db2dd171808726e78e61b752f43d46f3b
Nicholas Asimov <nicholas@asimov.me>
2016-08-28 20:00:35 +0300

    Facecast Settings Menu 0.1

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import {Events, Styler, UICorePlugin, template} from 'Clappr'
+import pluginHtml from './public/settings-menu.html'
+import pluginStyle from './public/style.scss'
+
+export default class SettingsMenu extends UICorePlugin {
+
+  static get version() { return VERSION }
+
+  get name() { return 'settings_menu' }
+  get template() { return template(pluginHtml) }
+
+  get attributes() {
+    return {
+      'class': this.name,
+      'data-settings-menu': ''
+    }
+  }
+
+  get events() {
+    return {
+      'click [data-settings-menu-button]': 'onShowSettingsMenu'
+    }
+  }
+
+  bindEvents() {
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render);
+  }
+
+  unBindEvents() {
+    this.stopListening(this.core, Events.MEDIACONTROL_RENDERED)
+
+  }
+
+  reload() {
+    this.unBindEvents()
+    this.bindEvents()
+  }
+
+  render() {
+    var style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl})
+
+    this.$el.html(this.template({'title': 'Settings Menu'}))
+    this.$el.append(style)
+    this.core.mediaControl.$('.media-control-right-panel').append(this.el)
+
+    return this
+  }
+
+  onShowSettingsMenu(event) { this.toggleContextMenu() }
+
+  hideSettingsMenu() { this.$('.settings_menu > .container').hide() }
+
+  toggleContextMenu() { this.$('.settings_menu > .container').toggle() }
+}
